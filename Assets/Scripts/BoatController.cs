015fa6069e0e309c009ea5052e6c90e11732e748
Daniil <105662394+MGS-Daniil@users.noreply.github.com>
2025-02-02 18:00:55 +0300

    Update BoatController.cs

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -1,3 +1,4 @@
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR.Content.Interaction;
 
@@ -5,6 +6,7 @@ public class BoatController : MonoBehaviour
 {
     private Rigidbody rigidbody;
     private bool isStarted;
+    private GameObject lightGameObject;
     public float horizontalSpeed = 5f;
     public float verticalSpeed = 20f;
     public Camera mainCamera, camera1;
@@ -12,25 +14,29 @@ public class BoatController : MonoBehaviour
     public XRJoystick joystick;
     public Transform panelTransform;
     public Transform standTransform;
-    
+    public GameObject controlPanel;
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        lightGameObject = controlPanel.transform.GetChild(8).gameObject;
     }
 
     void Update()
     {
         // var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = 0f;
+        var vertical = 0f;
         if (isStarted)
         {
+            horizontal = knob.value - 0.5f;
+            vertical = joystick.value.x;
         }
 
-        var horizontal = knob.value-0.5f;
-        var vertical = joystick.value.x;
 
         rigidbody.AddForce(transform.forward * vertical * verticalSpeed);
         rigidbody.AddTorque(transform.up * horizontal * horizontalSpeed);
-        
+
         panelTransform.position = standTransform.position;
         panelTransform.rotation = standTransform.rotation;
 
@@ -38,6 +44,12 @@ public class BoatController : MonoBehaviour
         // {
         //     SwitchCamera();
         // }
+    }
+
+    public void ChangeMotorState()
+    {
+        isStarted = !isStarted;
+        lightGameObject.SetActive(isStarted);
     }
 
     public void SwitchCamera()
