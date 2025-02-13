721140f39ad84cc5cf11cc3004d78ea394fc124b
Daniil <105662394+MGS-Daniil@users.noreply.github.com>
2025-02-13 20:04:12 +0300

    update control

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -12,8 +12,8 @@ public class BoatController : MonoBehaviour
     [SerializeField] private Transform panelTransform;
     [SerializeField] private Transform standTransform;
     [SerializeField] private GameObject controlPanel;
+    [SerializeField] private bool isStarted;
     private Rigidbody rigidbody;
-    private bool isStarted;
     private GameObject lightGameObject;
 
     void Start()
@@ -29,8 +29,8 @@ public class BoatController : MonoBehaviour
         
         if(!isStarted) return;
         // var horizontal = Input.GetAxis("Horizontal");
-        var horizontal = knob.value - 0.5f;
-        var vertical = joystick.value.y;
+        var vertical = -joystick.value.y;
+        var horizontal = (knob.value - 0.5f) * Mathf.Abs(vertical);
 
         if (vertical > 0.1f)
         {
@@ -39,8 +39,6 @@ public class BoatController : MonoBehaviour
         
         rigidbody.AddForce(transform.forward * vertical * verticalSpeed);
         rigidbody.AddTorque(transform.up * horizontal * horizontalSpeed);
-
-      
 
         // if (Input.GetKeyDown(KeyCode.C))
         // {
